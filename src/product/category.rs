446a63126bbec3e2a257c52f3344fa1cdfe4cb9b
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-30 19:16:10 +0400

    added test case

--- src/product/category.rs
+++ src/product/category.rs
@@ -1,6 +1,20 @@
+#[derive(Debug, PartialEq)]
 pub enum Category {
     Electronics,
     Clothing,
     Food,
     Cosmetics,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_category() {
+        assert_eq!(Category::Electronics, Category::Electronics);
+        assert_eq!(Category::Clothing, Category::Clothing);
+        assert_eq!(Category::Food, Category::Food);
+        assert_eq!(Category::Cosmetics, Category::Cosmetics);
+    }
 }
