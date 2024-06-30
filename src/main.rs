167fa1187cb2b18a5edcde3a0efe2e8f664fb3ac
tahseen.jamal <tahseen.jamal@gmail.com>
2024-06-30 14:27:20 +0400

    updated

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,23 @@ use ecom::Tax;
 
 fn main() {
     let tahseen: Customer = Customer::new("Tahseen", 48, "tahseen.jamal@gmail.com", "India");
-    println!("Name: {}", tahseen.get_name());
 
     let product1: Product = Product::new(1, "Laptop", 500.0, Category::Electronics, Tax::VAT);
     let product2: Product = Product::new(2, "Shampoo", 20.0, Category::Cosmetics, Tax::GST);
-
-    let mut order: Order = Order::new(1, vec![product1, product2], Discount::Medium, tahseen);
-
-    println!("Total products: {}", order.get_total_products());
-    println!("Total products: {}", order.get_total());
-
     let product3 = Product::new(2, "Shampoo", 20.0, Category::Cosmetics, Tax::GST);
+
+    let mut order: Order = Order::new(tahseen);
+
+    order.add_product(product1);
+    order.add_product(product2);
     order.add_product(product3);
 
+    order.set_discount(Discount::Medium);
+
+    println!(
+        "Total unique products: {}",
+        order.get_total_unique_products()
+    );
     println!("Total products: {}", order.get_total_products());
-    println!("Total products: {}", order.get_total());
-    println!("Product count: {}", order.product_wise_count());
+    println!("Total cost: {}", order.get_total_price());
 }
